102b5552eb12eec4042ef1f4c40c08f570db4514
Lissandro <lissandromedeiros@gmail.com>
2022-07-05 19:51:46 -0300

    “7-correcao+-erro5”

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   // Mapeamento de teclas
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '+/-', '=']
+  const buttons = ['LIMPAR', 'DEL', '+/-',  '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0,'.', '=']
 
   const [currentNumber, setCurrentNumber] = useState("")
   const [lastNumber, setLastNumber] = useState("")
@@ -24,7 +24,7 @@ export default function App() {
       case '-': 
         setCurrentNumber((fistNumber - lastNumber).toString())
         return
-      case 'x':
+      case '*':
         setCurrentNumber((fistNumber * lastNumber).toString())
         return
       case '/': 
@@ -35,7 +35,7 @@ export default function App() {
 
   function handleInput(buttonPressed){
     console.log(buttonPressed) // Mostra no Console a tecla pressionada
-    if(buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "x" | buttonPressed === "/" ){
+    if(buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "*" | buttonPressed === "/" ){
       setCurrentNumber(currentNumber + " " + buttonPressed + " ")
       return
     }
@@ -52,6 +52,7 @@ export default function App() {
         calculator()
         return
       case '+/-':
+        setCurrentNumber((currentNumber * -1).toString())
         return
     }
 
@@ -119,6 +120,7 @@ const styles = StyleSheet.create({
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
             <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
           </TouchableOpacity>
+          
         )}
       </View>
     </View>
